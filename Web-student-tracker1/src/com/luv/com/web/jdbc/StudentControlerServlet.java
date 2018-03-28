4cb7470d1a3982dd08a3b6796ae9a1d03125294a
Vitor Gil <vgc_silva@hotmail.com>
2018-03-28 11:55:57 +0100

    Config DispatcherServer

--- Web-student-tracker1/src/com/luv/com/web/jdbc/StudentControlerServlet.java
+++ Web-student-tracker1/src/com/luv/com/web/jdbc/StudentControlerServlet.java
@@ -1,8 +1,11 @@
 package com.luv.com.web.jdbc;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.DispatcherType;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,18 +20,50 @@ import javax.sql.DataSource;
 public class StudentControlerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	
 	private StudentDbUtil studentDbUtil;
 	
 	@Resource(name="jdbc/web_student_tracker")
 	private DataSource dataSource;
 	
 	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		//create our student db util... and pass in the conn pool/datasource
+		try {
+			studentDbUtil = new StudentDbUtil(dataSource);
+		}
+		catch(Exception exc) {
+			throw new ServletException(exc);
+		}
+		
+	}
+
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//list the student ... in MVC fashion
+		try {
+		listStudents(request, response);
+		}
+		catch(Exception exc) {
+			throw new ServletException(exc);
+		}	
+	}
+
+
+	private void listStudents(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//get students from db util
+		List<Student> students = studentDbUtil.getStudent();
+		//add students to the request
+		request.setAttribute("STUDENT_LIST", students);
+		
+		//send to JSP page (view)
+		RequestDispatcher dispathecher = request.getRequestDispatcher("/list-student.jsp");
+		dispathecher.forward(request, response);
 	}
 
 }
